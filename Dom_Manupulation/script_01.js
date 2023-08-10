8eaaebf4672f74eb57ec7921a30ce94ff0261f68
vinaypandey2017 <vinaypandey0398@gmail.com>
2023-08-10 20:52:06 +0530

    BackGround Green and 3rd item Invisible

---
+++ Dom_Manupulation/script_01.js
@@ -0,0 +1,20 @@
+
+// const itemsList = document.getElementById("items");
+// let  itemsList=document.getElementsByClassName("list-group");
+
+// const newItem = document.createElement("li");
+// newItem.className = "list-group-item"
+
+// newItem.textContent = "item 5";
+// itemsList.appendChild(newItem);
+// var nthChild=document.querySelector('items');
+
+// nthChild.style.backgroundColor='yellow';
+// let listElm=document.getElementsByClassName("list-group")
+
+// listElm[1].computedStyleMap.backgroundColor="yellow";
+// let ul=document.querySelectorAll(".list-group");
+// ul[1].innerHTML=<h3>Hello</h3>;
+var ul = document.querySelectorAll(".list-group-item");
+ul[1].style.backgroundColor='green'
+ul[2].textContent='';
